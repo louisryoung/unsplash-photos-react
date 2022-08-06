256fabf9c591cbe9fd5fff4d1f2a6f7675d2a491
Louis Young <louis.young0420@gmail.com>
2022-08-07 00:09:06 +0900

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,40 @@ import { unsplashApi } from './utils/api'
 import 'react-bnb-gallery/dist/style.css'
 import './styles/App.css'
 
-type Photo = {
+type ResponsePhoto = {
   id: string
-  alt_description?: string
   width: number
   height: number
-  created_at: string
   description: string
+  alt_description: string
   urls: {
-    small: string
     thumb: string
+    small: string
   }
 }
 
+type MasonryPhoto = {
+  width: number
+  height: number
+  key: string
+  src: string
+  alt: string
+}
+
+type LightboxPhoto = {
+  number: number
+  key: string
+  photo: string
+  thumbnail: string
+  caption: string
+  subcaption: string
+}
+
 function App() {
-  const [photos, setPhotos] = React.useState<Photo[]>([])
+  const [masonryPhotos, setMasonryPhotos] = React.useState<MasonryPhoto[]>([])
+  const [lightboxPhotos, setLightboxPhotos] = React.useState<LightboxPhoto[]>(
+    []
+  )
   const [isLoading, setIsLoading] = React.useState(false)
   const [page, setPage] = React.useState(1)
   const [currentImage, setCurrentImage] = React.useState(0)
@@ -40,7 +59,7 @@ function App() {
     ;(async function () {
       try {
         setIsLoading(true)
-        const { response } = await unsplashApi.photos.getRandom({
+        const { response } = (await unsplashApi.photos.getRandom({
           count: 12,
           topicIds: [
             '6sMVjTLSkeQ',
@@ -50,8 +69,34 @@ function App() {
             'iUIsnVtjB0Y',
             'aeu6rL-j6ew',
           ],
-        })
-        setPhotos((photos) => [...photos, ...(response as unknown as Photo[])])
+        })) as unknown as { response: ResponsePhoto[] }
+        const nextMasonryPhotos = response.map(
+          ({ id, urls, width, height, alt_description }) => ({
+            width,
+            height,
+            key: id,
+            src: urls.thumb,
+            alt: alt_description || 'Unsplash image',
+          })
+        )
+        setMasonryPhotos((previousPhotos) => [
+          ...previousPhotos,
+          ...nextMasonryPhotos,
+        ])
+        const nextLightboxPhotos = response.map(
+          ({ id, urls, description, alt_description }, i) => ({
+            number: i,
+            key: id,
+            photo: urls.small,
+            thumbnail: urls.thumb,
+            caption: description,
+            subcaption: alt_description,
+          })
+        )
+        setLightboxPhotos((previousPhotos) => [
+          ...previousPhotos,
+          ...nextLightboxPhotos,
+        ])
       } catch (err) {
         console.error(err)
       } finally {
@@ -95,33 +140,14 @@ function App() {
     }
   }
 
-  const photosToRender = React.useMemo(
-    () =>
-      photos.map(
-        ({ id, urls, width, height, description, alt_description }, i) => ({
-          width,
-          height,
-          number: i,
-          key: id,
-          src: urls.thumb,
-          thumbnail: urls.thumb,
-          photo: urls.small,
-          alt: alt_description || 'Unsplash image',
-          caption: description,
-          subcaption: alt_description,
-        })
-      ),
-    [photos]
-  )
-
   return (
     <>
       <Header></Header>
       <main>
-        <Gallery photos={photosToRender} onClick={openLightbox} />
+        <Gallery photos={masonryPhotos} onClick={openLightbox} />
         <ReactBnbGallery
           show={isOpen}
-          photos={photosToRender}
+          photos={lightboxPhotos}
           onClose={closeLightbox}
           activePhotoIndex={currentImage}
         />
