294d70f399330ac8bf77077ddb1234d7f5455a82
Louis Young <louis.young0420@gmail.com>
2022-08-06 12:16:10 +0900

    add: LoadingSpinner

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,15 @@
+import logo from '../logo.svg'
+
+type Props = {
+  isLoading: boolean
+}
+
+function LoadingSpinner({ isLoading }: Props) {
+  return isLoading ? (
+    <div className="loading-container">
+      <img src={logo} className="loading-spinner" alt="loading spinner" />
+    </div>
+  ) : null
+}
+
+export { LoadingSpinner }
