429c26615331e14ee4d462ef04ea5b7c8a4a9b30
Louis Young <louis.young0420@gmail.com>
2022-08-06 12:16:24 +0900

    fixed: file structure

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import { createApi } from 'unsplash-js'
 
-import { accessKey } from '../../constants'
+import { accessKey } from '../constants'
 
 export const unsplashApi = createApi({
   accessKey,
